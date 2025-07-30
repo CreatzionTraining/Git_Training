7db7a2b3c54b6a9d8411b492c0904310c8d4162f
Sathiya2144 <sathiya8747@gmail.com>
2025-07-30 16:32:20 +0530

    sakthi wedding

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -6,7 +6,7 @@ function Upload() {
   
   return (
     <div className='p-5 px-8  md:px-28'>
-      <h2 className='text[20px] text-center  m-5'>  Start <strong className='text-primary'>Sakthi </strong>  File and <strong className='text-primary'>Share </strong>it</h2>
+      <h2 className='text[20px] text-center  m-5'>  Start <strong className='text-primary'>sakthi weds navya </strong>  File and <strong className='text-primary'>Share </strong>it</h2>
       <UploadForm/> 
     </div>
   )
